f8ccf7b15cf0710b0c30bf0c9f4fd98a8f0ee358
freitzzz <1160907@isep.ipp.pt>
2020-10-24 23:12:43 +0100

    XML deserialization to UserInfo model

--- lib/src/models/user_info.dart
+++ lib/src/models/user_info.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+import 'package:xml/xml.dart';
 
 import 'models.dart';
 
@@ -32,4 +33,24 @@ class UserInfo {
     @required this.visibility,
     @required this.website,
   });
+
+  static UserInfo fromXmlNode(final XmlNode xmlNode) {
+    return UserInfo(
+      avatarUrl: Uri.tryParse(xmlNode.getElement('user_avatar_url')?.text),
+      email: xmlNode.getElement('user_email')?.text,
+      expiration: ExpireDateExtension.tryParse(
+        xmlNode.getElement('user_expiration')?.text,
+      ),
+      format: FormatExtension.tryParse(
+        xmlNode.getElement('user_format_short')?.text,
+      ),
+      isPro: xmlNode.getElement('user_format_short')?.text?.endsWith('1'),
+      location: xmlNode.getElement('user_location')?.text,
+      userName: xmlNode.getElement('user_name')?.text,
+      visibility: VisibilityExtension.tryParse(
+        xmlNode.getElement('user_private')?.text,
+      ),
+      website: xmlNode.getElement('user_website')?.text,
+    );
+  }
 }
