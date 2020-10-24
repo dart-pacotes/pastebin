66ee29b55a748d240fb84dc889520e97612ae5b8
freitzzz <1160907@isep.ipp.pt>
2020-10-24 19:13:19 +0100

    Type pastebin client API with Future

--- lib/src/pastebin_client.dart
+++ lib/src/pastebin_client.dart
@@ -4,29 +4,29 @@ import 'package:meta/meta.dart';
 import 'models/models.dart';
 
 abstract class PastebinClient {
-  Either<void, RequestError> paste({
+  Future<Either<void, RequestError>> paste({
     @required final String pasteText,
     final PasteOptions options,
   });
 
-  Either<String, RequestError> apiUserKey({
+  Future<Either<String, RequestError>> apiUserKey({
     @required final String username,
     @required final String password,
   });
 
-  Either<List<Paste>, RequestError> pastes({
+  Future<Either<List<Paste>, RequestError>> pastes({
     final int limit = 50,
   });
 
-  Either<void, RequestError> delete({
+  Future<Either<void, RequestError>> delete({
     @required final String pasteKey,
   });
 
-  Either<UserInfo, RequestError> userInfo({
+  Future<Either<UserInfo, RequestError>> userInfo({
     @required final String userKey,
   });
 
-  Either<String, RequestError> rawPaste({
+  Future<Either<String, RequestError>> rawPaste({
     @required final String pasteKey,
     @required final Visibility visibility,
   });
